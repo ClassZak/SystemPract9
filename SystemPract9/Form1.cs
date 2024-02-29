bc27ebe7c3855988e299ca07865d375211556530
Захар Голуб <pahargolub2@gmail.com>
2024-02-29 21:15:50 +0300

    Добавьте файлы проекта.

---
+++ SystemPract9/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemPract9
+{
+    class Logger
+    {
+        
+        protected byte[] stringToBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length / 2];
+
+            for (int i = 0; i != str.Length; i += 2)
+                bytes[i / 2] = (byte)str[i];
+            return bytes;
+        }
+
+        static public bool AppRunning;
+        static public void HandleEndOfApp(object sender, EventArgs e)
+        {
+            AppRunning=false;
+            LogRunning();
+        }
+        static public void HandleBeginOfApp(object sender, EventArgs e)
+        {
+            AppRunning = true;
+            LogRunning();
+        }
+        public static void LogRunning()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo("Logs");
+            bool haveCurrLog = false;
+            string desiredName="Log"+DateTime.Now.Date.ToShortDateString()+".txt";
+            try
+            {
+                foreach(FileInfo file in directoryInfo.GetFiles("*.*"))
+                {
+                    if(file.Name==desiredName)
+                    {
+                        haveCurrLog = true;
+                        break;
+                    }
+                }
+            }
+            catch { }
+            
+
+            FileStream fileStream=new FileStream("Logs\\\\"+desiredName, haveCurrLog ? FileMode.Open : FileMode.Create, FileAccess.ReadWrite,FileShare.Write);
+            StreamWriter fileWriter = new StreamWriter(fileStream);
+            fileWriter.BaseStream.Seek(0, SeekOrigin.End);
+            fileWriter.Write(DateTime.Now.ToShortTimeString()+"\t");
+            fileWriter.WriteLine(AppRunning ? "Application started" : "Application Finished");
+            
+            fileWriter.Close();
+            //fileStream.Close();
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox3.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox3.Text = textBox1.Text + textBox2.Text;
+        }
+    }
+}
---
+++ SystemPract9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemPract9
+{
+    internal static class Program
+    {
+        static void FinishProgram()
+        {
+            if (EndProgram != null)
+                EndProgram(null,null);
+        }
+        static void StartProgram()
+        {
+            if (BeginProgram != null)
+                BeginProgram(null, null);
+        }
+        static public event EventHandler EndProgram;
+        static public event EventHandler BeginProgram;
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            EndProgram += new EventHandler(Logger.HandleEndOfApp);
+            BeginProgram += new EventHandler(Logger.HandleBeginOfApp);
+            StartProgram();
+            Application.Run(new Form1());
+            FinishProgram();
+        }
+    }
+}
